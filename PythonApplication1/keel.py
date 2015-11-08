4b8231f6ebe3af8990141cae6d17a3d1d0cefe00
Håkon <hakonsol@gmail.com>
2015-11-08 17:50:16 +0100

    Nokre endringar

--- PythonApplication1/keel.py
+++ PythonApplication1/keel.py
@@ -12,13 +12,21 @@ from Helpers import show
 # Use the following to render your model with grey RGB and no transparency
 # show(my_model, (204, 204, 204, 0.0))
 
-result = cadquery.Workplane("front").box(4.0, 4.0, 0.25).faces(">Z") \
-                 .circle(1.5).workplane(offset=3.0) \
-                 .rect(0.75, 0.5).loft(combine=True)
+#result = cadquery.Workplane("front").box(4.0, 4.0, 0.25).faces(">Z") \
+#                 .circle(1.5).workplane(offset=3.0) \
+#                 .rect(0.75, 0.5).loft(combine=True)
 
+
+keelprofile = Workplane("XY").lineTo(7,0).lineTo(7,1.5).threePointArc((1,0.5),(0,0)).close()
 
 #keelstock = Workplane("XY").workplane(offset=(50)).box(7,4,50)
-keelstock = Workplane("XY").box(7,4,50).faces(">Z").rect(1,4).extrude(-50)
+keelstock = Workplane("XY").box(7,4,50) #.faces(">Z").keelprofile.extrude(-25)
 #.workplane(offset=-50)                 .rect(0.75, 0.5).extrude(0.5)
 
-show(keelstock)
+#keel = keelstock.faces(">Z").add(keelprofile).extrude(-25)
+
+keel = Workplane("XY").box(7,4,50).faces(">Z").vertices("<XY").workplane() \
+        .lineTo(7,0).lineTo(7,1.5).threePointArc((1,0.5),(0,0)).close() \
+            .workplane().extrude(50.0)
+
+show(keel)
